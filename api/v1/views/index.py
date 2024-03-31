d679b5ee993b2debe43bef870b256b87c85f4065
NjengaC <victorcyrus01@gmail.com>
2024-03-31 23:47:06 +0300

    jsnify

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -16,9 +16,7 @@ from models.review import Review
 def status():
     """Endpoint that retrieves the status"""
 
-    result = {
-        "status": "OK"
-    }
+    result = {"status": "OK"}
     return jsonify(result)
 
 
