3a2d5622372d912408c3c7b8e58cd6563d7e0fe4
NjengaC <victorcyrus01@gmail.com>
2024-03-31 23:29:41 +0300

    index.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,34 +1,35 @@
 #!/usr/bin/python3
 """
-Main docs for app module
+Starts a Flask web application
 """
 
 from os import getenv
-from flask import Flask, Blueprint, jsonify
-from models import storage
+from flask import Flask, jsonify
+from flask_cors import CORS
 from api.v1.views import app_views
+from models import storage
 
 app = Flask(__name__)
-app.register_blueprint(app_views)
+
+app.register_blueprint(app_views, url_prefix="/api/v1")
+CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
-def teardown_appcontext(exception):
-    """
-    Closes current session
-    """
+def teardown_db(exception):
+    """Closes the database again at the end of the request."""
     storage.close()
 
 
 @app.errorhandler(404)
-def error_404(error):
-    """
-    Handles custorm json(404)
-    """
-    return jsonify({'error': 'Not found'}), 404
+def not_found(error):
+    """Error handler for 404 not found."""
+    return jsonify({"error": "Not found"}), 404
 
 
 if __name__ == "__main__":
-    host = getenv("HBNB_API_HOST")
-    port = int(getenv("HBNB_API_PORT"))
-    app.run(host=host, port=port, threaded=True, debug=True)
+
+    host = getenv("HBNB_API_HOST", "0.0.0.0")
+    port = int(getenv("HBNB_API_PORT", 5000))
+
+    app.run(host=host, port=port, threaded=True)
