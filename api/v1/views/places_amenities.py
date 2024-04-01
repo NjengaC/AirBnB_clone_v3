cd3056181b9730d09c36938f33f828ea6c114559
NjengaC <victorcyrus01@gmail.com>
2024-04-01 15:24:43 +0300

    places_amenities

---
+++ api/v1/views/places_amenities.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python3
+"""
+Module handles all default RESTFul API actions:
+"""
+from flask import Flask, jsonify, abort, request
+from models import storage, storage_t
+from models.place import Place
+from models.amenity import Amenity
+from api.v1.views import app_views
+
+
+@app_views.route('/places/<place_id>/amenities',
+                 methods=['GET'], strict_slashes=False)
+def get_place_amenities(place_id):
+    """
+    Retrieves the list of all Amenity objects of a Place
+    """
+    place = storage.get(Place, place_id)
+    if not place:
+        abort(404)
+    amenities = [amenity.to_dict() for amenity in place.amenities]
+    return jsonify(amenities), 200
+
+
+@app_views.route('/places/<place_id>/amenities/<amenity_id>',
+                 methods=['DELETE'], strict_slashes=False)
+def delete_place_amenity(place_id, amenity_id):
+    """
+    Deletes a Amenity object from a Place
+    """
+    place = storage.get(Place, place_id)
+    if not place:
+        abort(404)
+    amenity = storage.get(Amenity, amenity_id)
+    if not amenity or amenity not in place.amenities:
+        abort(404)
+    if storage_t == "db":
+        place.amenities.remove(amenity)
+    else:
+        place.amenity_ids.remove(amenity_id)
+    place.save()
+    return jsonify({}), 200
+
+
+@app_views.route('/places/<place_id>/amenities/<amenity_id>',
+                 methods=['POST'], strict_slashes=False)
+def link_place_amenity(place_id, amenity_id):
+    """
+    create a Amenity object from a Place
+    """
+    place = storage.get(Place, place_id)
+    if not place:
+        abort(404)
+    amenity = storage.get(Amenity, amenity_id)
+    if not amenity:
+        abort(404)
+    if amenity in place.amenities:
+        return jsonify(amenity.to_dict()), 200
+    if storage_t == "db":
+        place.amenities.append(amenity)
+    else:
+        place.amenity_ids.append(amenity_id)
+    place.save()
+    return jsonify(amenity.to_dict()), 201
